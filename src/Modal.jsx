ba183c3dd8594d13fa710cdd7bdf575ba1116d54
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-11 12:12:00 -0500

    add modal component structure and css

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,17 @@
+import { FaTimes } from 'react-icons/fa';
+import { useCustomHook } from './context';
+
 const Modal = () => {
-  return <div>Modal</div>;
+  const { isModalOpen, closeModal } = useCustomHook();
+  return (
+    <div className={isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}>
+      <div className="modal-container">
+        <h2>modal content</h2>
+        <button className="modal-close-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Modal;
