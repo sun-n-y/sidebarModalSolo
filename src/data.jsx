9e011e37183ae513556a3844d9f3a9ea7dbdf99c
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-11 11:28:56 -0500

    add data file for links and icons

---
+++ src/data.jsx
@@ -0,0 +1,66 @@
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaYoutube,
+  FaBehance,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+} from 'react-icons/fa';
+
+export const links = [
+  {
+    id: 1,
+    url: '#',
+    text: 'home',
+    icon: <FaHome />,
+  },
+  {
+    id: 2,
+    url: '#',
+    text: 'about',
+    icon: <FaUserFriends />,
+  },
+  {
+    id: 3,
+    url: '#',
+    text: 'contact',
+    icon: <FaFolderOpen />,
+  },
+  {
+    id: 4,
+    url: '#',
+    text: 'page',
+    icon: <FaCalendarAlt />,
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: '#',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: '#',
+    icon: <FaBehance />,
+  },
+  {
+    id: 3,
+    url: '#',
+    icon: <FaInstagram />,
+  },
+  {
+    id: 4,
+    url: '#',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 5,
+    url: '#',
+    icon: <FaTwitter />,
+  },
+];
