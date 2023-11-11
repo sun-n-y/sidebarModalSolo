37034bce059a1a77b982b4a3167c2ec7dd8c9fbb
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-11 11:58:56 -0500

    add home component structure and css

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,17 @@
+import { FaBars } from 'react-icons/fa';
+import { useCustomHook } from './context';
+
 const Home = () => {
-  return <div>Home</div>;
+  const { openSidebar, openModal } = useCustomHook();
+  return (
+    <main>
+      <button className="sidebar-btn" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="modal-btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
 };
 export default Home;
