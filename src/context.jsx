d1faa78f03efb90874c19e29530642e20049f2e6
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-11 11:38:20 -0500

    add global context api setup

---
+++ src/context.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from 'react';
+
+const GlobalContext = createContext();
+
+const AppContext = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default AppContext;
